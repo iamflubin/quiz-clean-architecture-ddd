3e86c8fa9a9e5437c880aaf874308a6143be7447
iamflubin <f.lubin@outlook.fr>
2025-01-19 03:50:22 +0100

    chore: add spring boot backend project

---
+++ backend/src/main/java/org/iamflubin/quizz/BackendApplication.java
@@ -0,0 +1,24 @@
+package org.iamflubin.quizz;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaAuditing
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+}
---
+++ backend/src/test/java/org/iamflubin/quizz/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iamflubin.quizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
